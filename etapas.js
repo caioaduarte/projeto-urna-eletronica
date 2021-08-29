aea29ed5e594bcbbfa30de957495bb332dd72dbe
Caio Duarte <caio1903@gmail.com>
2021-08-29 20:48:37 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Batman',
+        partido: 'XYZ',
+        fotos:[
+          {url: 'https://upload.wikimedia.org/wikipedia/pt/8/8d/Batman_por_Jim_Lee.jpg', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: '77222',
+        nome: 'Wolverine',
+        partido: 'X-MEN',
+        fotos:[
+          {url: 'https://upload.wikimedia.org/wikipedia/pt/thumb/c/c8/Marvelwolverine.jpg/200px-Marvelwolverine.jpg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '90',
+        nome: 'Dr. House',
+        partido: 'ABC',
+        fotos:[
+          {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Hugh_Laurie_2009.jpg/219px-Hugh_Laurie_2009.jpg', legenda: 'Prefeito'},
+          {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Omar_Epps.jpg/240px-Omar_Epps.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'Jackie Chan',
+        partido: 'DEF',
+        fotos:[
+          {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Jackie_Chan_July_2016.jpg/250px-Jackie_Chan_July_2016.jpg', legenda: 'Prefeito'},
+          {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Bruce_Lee_1973.jpg/230px-Bruce_Lee_1973.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
